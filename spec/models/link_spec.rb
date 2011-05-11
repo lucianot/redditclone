9391473d17761ed887394a407159299e43a4a9d5
luciano <lucianomt@gmail.com>
2011-05-11 09:25:52 -0300

    Refactored model tests

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,21 +1,22 @@
 require 'spec_helper'
 
 describe Link do
+  before(:each) do
+    @link = Link.new(:title => "This guy copied my site", 
+                     :url => "www.reddit.com")
+  end
+  
   it 'is valid with valid attributes' do
-    link = Link.new :title => "This guy copied my site", 
-                    :url => "www.reddit.com"
-    link.should be_valid
+    @link.should be_valid
   end
   
   it 'is invalid without a title' do
-    link = Link.new :title => nil, 
-                    :url => "www.reddit.com"
-    link.should_not be_valid
+    @link.title = nil
+    @link.should_not be_valid
   end
 
   it 'is invalid without a url' do
-    link = Link.new :title => "This guy copied my site", 
-                    :url => nil
-    link.should_not be_valid
+    @link.url = nil
+    @link.should_not be_valid
   end
 end
